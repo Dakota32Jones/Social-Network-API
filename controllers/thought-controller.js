1a4e2a11e76ece0fb9fbefa1f85391407dc0eea1
Dakota Jones <dakota32jones@gmail.com>
2022-10-22 19:18:34 -0500

    get all thoughts controllers method

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+const { Thought, User, Types } = require("../models");
+
+// get all thoughts
+const ThoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
