cebe456da36b749b0fb2e56d33880fba97958d0d
Dakota Jones <dakota32jones@gmail.com>
2022-10-22 20:01:13 -0500

    finished thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -70,4 +70,32 @@ const ThoughtController = {
       .catch((err) => res.json(err));
   },
   // add reaction to user by ID
+  addReaction({ params, body }, res) {
+    console.log("INCOMING BODY", body);
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+  // remove reaction
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 };
+
+module.exports = ThoughtController;
