9a92a023cb3b7b21e3a5e002fc988494f0f9e610
Dakota Jones <dakota32jones@gmail.com>
2022-10-22 19:25:24 -0500

    added thought by ID controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,4 +12,22 @@ const ThoughtController = {
         res.status(400).json(err);
       });
   },
+  //   get thoughts by ID
+  getThoughtById({ params }, res) {
+    console.log("params sent", params);
+    Thought.findOne({ _id: params.thoughtId })
+      .select("-__v")
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // add a thought to a user
 };
